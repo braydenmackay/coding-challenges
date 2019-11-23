6f76a24b550b102b8dfd75dc6e168c82a0a9c08f
braydenmackay <braydenmackay1@gmail.com>
2019-11-23 10:05:06 -0800

    new challenge

--- app.py
+++ app.py
@@ -198,3 +198,10 @@ def summation(num):
   return count
 
 summation(6)
+
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+def make_negative( number ):
+  if number > 0:
+    return (number * -1)
+  else:
+    return number
