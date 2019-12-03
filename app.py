cc1431fb656b00c5b25639b76b72065b9ffb2170
braydenmackay <braydenmackay1@gmail.com>
2019-12-03 11:11:31 -0700

    new challenge

--- app.py
+++ app.py
@@ -273,3 +273,13 @@ def friend(x):
     
 
 friend(['kyle', 'brayden', 'tim', 'cole'])
+
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+def bool_to_word(boolean):
+  if boolean == True:
+    return 'Yes'
+  else:
+    return 'No'
+
+bool_to_word(True)
+
