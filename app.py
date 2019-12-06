44291b33d81966304c59ad3836744a158a6f51e6
braydenmackay <braydenmackay1@gmail.com>
2019-12-05 17:19:13 -0700

    new challenge

--- app.py
+++ app.py
@@ -298,3 +298,13 @@ def xo(s):
     return False
 
 xo('xxoOxo')
+
+# create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+def filter_list(l):
+  num_arr = []
+  for char in l:
+    if type(char) == int:
+      num_arr.append(char)
+  return num_arr
+
+filter_list([1,2,3,'a','b',3,5])
