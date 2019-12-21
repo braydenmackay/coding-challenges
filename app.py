cf7399d822abd050cdb817e9ceb9710f00e42450
braydenmackay <braydenmackay1@gmail.com>
2019-12-21 16:58:24 -0700

    new challenge

--- app.py
+++ app.py
@@ -409,3 +409,10 @@ def div_con(x):
 def sum_two_smallest_numbers(numbers):
   numbers.sort()
   return numbers[0] + numbers[1]
+
+# You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1. As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+def monkey_count(n):
+  my_arr = []
+  for i in range(1,n+1):
+    my_arr.append(i)
+  return my_arr
