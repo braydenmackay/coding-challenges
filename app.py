dce7d6a9d3b155a6832353fcb67427b9e1012ee5
braydenmackay <braydenmackay1@gmail.com>
2020-01-11 12:51:30 -0700

    new challenge

--- app.py
+++ app.py
@@ -497,3 +497,7 @@ def get_sum(a,b):
   for num in range(min(arr), max(arr) + 1):
     new_arr.append(num)
   return sum(new_arr)
+
+# Write a method sum (sum_array in python, sumarray in julia, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. If the array does not contain any numbers then you should return 0.
+def sum_array(a):
+  return sum(a)
